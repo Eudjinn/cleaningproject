d3a9431602e6199271b2281efad297e7fbd515f5
Eudjinn <eugeneios@gmail.com>
2015-10-11 14:51:03 +0300

    changed identifier names to meet a better naming convension for R (Google-based) and changed column names transformations

--- run_analysis.R
+++ run_analysis.R
@@ -1,71 +1,70 @@
-run_analysis <- function() {
+runAnalysis <- function() {
     print("Started the analysis, please wait...")
-    ## Read the measurements
-    test_X <- read.table("./UCI HAR Dataset/test/X_test.txt")
-    train_X <- read.table("./UCI HAR Dataset/train/X_train.txt")
+
+    # Read the measurements
+    test.x <- read.table("./UCI HAR Dataset/test/X_test.txt")
+    train.x <- read.table("./UCI HAR Dataset/train/X_train.txt")
     
     features <- read.delim("./UCI HAR Dataset/features.txt", 
                            sep = " ", 
                            header = FALSE, 
-                           col.names = c("fature_id", "feature_name"))
+                           col.names = c("fatureId", "featureName"))
     
     activities <- read.delim("./UCI HAR Dataset/activity_labels.txt",
                              sep = " ", 
                              header = FALSE, 
-                             col.names = c("activity_id", "activity_name"))
+                             col.names = c("activityId", "activityName"))
     
-    measures_X <- bind_rows(train_X, test_X)
+    measures.x <- bind_rows(train.x, test.x)
     # unique column names are needed in order for the select from dplyr library to work
-    names(measures_X) <- make.names(features$feature_name, unique = TRUE)
+    names(measures.x) <- make.names(features$featureName, unique = TRUE)
     
-    ## Read activities
-    test_y <- read.table("./UCI HAR Dataset/test/y_test.txt")
-    train_y <- read.table("./UCI HAR Dataset/train/y_train.txt")
-    measures_y <- bind_rows(train_y, test_y)
-    names(measures_y) <- "activity_id"
+    # Read activities
+    test.y <- read.table("./UCI HAR Dataset/test/y_test.txt")
+    train.y <- read.table("./UCI HAR Dataset/train/y_train.txt")
+    measures.y <- bind_rows(train.y, test.y)
+    names(measures.y) <- "activityId"
     
-    ## subject ids
-    test_sub <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-    train_sub <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-    measures_sub <- bind_rows(test_sub, train_sub)
-    names(measures_sub) <- "subject_id"
+    # subject ids
+    test.subject <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+    train.subject <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+    measures.subject <- bind_rows(test.subject, train.subject)
+    names(measures.subject) <- "subjectId"
     
-    ## put all measurements into one data frame
-    complete_X <- bind_cols(measures_sub, measures_y, measures_X)
+    # put all measurements into one data frame
+    complete.x <- bind_cols(measures.subject, measures.y, measures.x)
     
-    ## take only mean and std measurements
-    selected_X <- select(complete_X, subject_id, activity_id, matches("\\.(mean|std)\\."))
+    # take only mean and std measurements
+    selected.x <- select(complete.x, subjectId, activityId, matches("\\.(mean|std)\\."))
     
-    ## replace activity_id with activity_name using join.
-    joined_X <- selected_X %>% 
-        left_join(activities, by = "activity_id") %>%
-        select(-activity_id)
+    # replace activityId with activityName using join.
+    joined.x <- selected.x %>% 
+        left_join(activities, by = "activityId") %>%
+        select(-activityId)
     
-    ## column "activity_name" is the last in the column list after join, 
-    ## it is better to rearrange the order of columns
-    X <- bind_cols(select(joined_X, subject_id), 
-                   select(joined_X, activity_name), 
-                   select(joined_X, -subject_id, -activity_name))
+    # column "activityName" is the last in the column list after join, 
+    # it is better to rearrange the order of columns
+    x <- bind_cols(select(joined.x, subjectId), 
+                   select(joined.x, activityName), 
+                   select(joined.x, -subjectId, -activityName))
     
-    ## Make better names for the features
-    ## Replace first dot with _
-    names(X) <- sub("\\.", "_", names(X))
-    ## Deal with ... replacing it by _ 
-    names(X) <- sub("\\.\\.\\.", "_", names(X))
-    ## Remove .. at the end of some feature names. 
-    ## The $ sign is redundant but let it be here indicating end of line. 
-    names(X) <- sub("\\.\\.$", "", names(X))
+    # Make better names for the features
+    # Remove all dots
+    names(x) <- gsub("\\.", "", names(x))
+    # mean to Mean and std to Std
+    names(x) <- gsub("mean", "Mean", names(x))
+    names(x) <- gsub("std", "Std", names(x))
     
-    ## calculate average of all the variables grouped by subject_id and activity_name
-    avg_grouped_X <- ddply(X, .(subject_id, activity_name), numcolwise(mean))
+    # calculate average of all the variables grouped by subjectId and activityName
+    avg.grouped.x <- ddply(x, .(subjectId, activityName), numcolwise(mean))
     
-    ## Modify names to reflect the average operation that has just been applied, 
-    ## we skip subject_id and activity_name
-    nl <- length(avg_grouped_X)
-    names(avg_grouped_X)[3:nl] <- paste0("Avg_", names(avg_grouped_X)[3:nl])
+    # Modify names to reflect the average operation that has just been applied, 
+    # we skip subjectId and activityName
+    names.length <- length(avg.grouped.x)
+    names(avg.grouped.x)[3:names.length] <- paste0(names(avg.grouped.x)[3:names.length], "Avg")
     
-    ## save result
-    write.table(avg_grouped_X, "tidy_dataset.txt", row.names = FALSE)
+    # save result
+    write.table(avg.grouped.x, "tidy_dataset.txt", row.names = FALSE)
     
     print("Done!")
 }
